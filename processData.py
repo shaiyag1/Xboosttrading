f433c3436783273e037b70885b2c4b1bc46f63ff
shai <shai.yag@gmail.com>
2017-10-11 15:59:18 +0300

    Add config.ini support

--- processData.py
+++ processData.py
@@ -20,16 +20,17 @@ from sklearn.metrics import confusion_matrix
 
 import pandas as pa
 import matplotlib.pyplot as plt
+import configparser
 
 from createfeaturesOnTop import createfeaturesOnTop
 
 
 
-def testSellOpportunityEX (sampleIndex,historicalDataPA):
+def testBuyOpprtunity (sampleIndex,historicalDataPA):
  
     sellThresh = 0.99
-    ROItresh  =1.09
-    futurerange=12*4
+    ROItresh  =1.07
+    futurerange=12*5
 
     bid = historicalDataPA[sampleIndex]
     if bid == 0:
@@ -65,6 +66,33 @@ def testSellOpportunityEX (sampleIndex,historicalDataPA):
         return 0
     #ind buy point %s  %s  %s ",i,bid,historicalDataPA[max_index_arr])
     return 1
+
+
+def testSellOpprtunity (sampleIndex,historicalDataPA):
+ 
+    sellThresh = 0.97
+    ROItresh  =1.04
+    futurerange=12*4
+
+    bid = historicalDataPA[sampleIndex]
+    if bid == 0:
+        return 0
+
+
+    #closedata = pa.DataFrame(historicalData)
+    #closedataarray = closedata['close'].as_matrix()
+    till =min(sampleIndex+futurerange,np.size(historicalDataPA))
+    if(till<0.5*futurerange):
+        return 0
+    testrange=10
+    for i in xrange(sampleIndex,till-testrange,testrange):
+        if(np.min(historicalDataPA[i:i+testrange])< sellThresh*bid):
+            return 1
+        if(np.max(historicalDataPA[i:i+testrange])> sellThresh*bid):
+            return 0
+    return 0
+
+
 
 
 
@@ -106,10 +134,10 @@ def coinstat(coin,dt):
 coinstat.counter = 0
 
 
-def createCoinDataEX(coin,startTime,endTime,mode='offline'):
+def createCoinDataEX(coin,startTime,endTime,mode='offline',buysellmode="Buy"):
         lastEma = 0
         historicalData = {}
-        movingPeriod = 200
+        movingPeriod = 210
         ticker=[]
 
 
@@ -126,6 +154,7 @@ def createCoinDataEX(coin,startTime,endTime,mode='offline'):
         tradedataMA=tradedata.as_matrix()
         if('offline' == mode):
             tradedataMA=tradedataMA[:,1:tradedataMA.shape[1]]
+        tradedataMA[:,1]=0
 
 
 
@@ -148,49 +177,31 @@ def createCoinDataEX(coin,startTime,endTime,mode='offline'):
 
 
         for   i in xrange (retdataset.shape[0]):
-
             ioffset=i+movingPeriod
-            j=8
-
-            if(True):
-                workingset,retmaxindex= createfeaturesOnTop(workingset,tradedataMA[ioffset,:])
-                workingset[0,retmaxindex]=testSellOpportunityEX (ioffset,tradedataMA[:,HistoricalDataOrderPandas['close'] ]  )
-                retdataset[i,:]=workingset[0,:]
-
-            else:
-                for item in ["quoteVolume","volume","weightedAverage",'close']:
-                    for k in [2,5,10,20,40,70]:
-                        retdataset[i,j]=np.max(tradedataMA[ioffset-k:ioffset,HistoricalDataOrderPandas[item]])
-                        j=j+1
-                        retdataset[i,j]=np.min(tradedataMA[ioffset-k:ioffset,HistoricalDataOrderPandas[item]])
-                        j=j+1
-                        retdataset[i,j]=np.mean(tradedataMA[ioffset-k:ioffset,HistoricalDataOrderPandas[item]])
-                        j=j+1
-                        retdataset[i,j]=np.min(tradedataMA[ioffset-k:ioffset,HistoricalDataOrderPandas[item]])-tradedataMA[ioffset,HistoricalDataOrderPandas[item]]
-                        j=j+1
-                        retdataset[i,j]=tradedataMA[ioffset][HistoricalDataOrderPandas[item]]-tradedataMA[i-k][HistoricalDataOrderPandas[item]]
-                        j=j+1
-                y = testSellOpportunityEX (ioffset,tradedataMA[:,HistoricalDataOrderPandas['close'] ]  )
-                retdataset[i,j]=y
-                retmaxindex=j
+            workingset,retmaxindex= createfeaturesOnTop(workingset,tradedataMA[ioffset,:])
+            if(buysellmode=="Buy"):
+                workingset[0,retmaxindex]=testBuyOpprtunity (ioffset,tradedataMA[:,HistoricalDataOrderPandas['close'] ]  )
+            if(buysellmode=="Sell"):
+                workingset[0,retmaxindex]=testSellOpprtunity (ioffset,tradedataMA[:,HistoricalDataOrderPandas['close'] ]  )
+            retdataset[i,:]=workingset[0,:]
 
 
         return retdataset,retmaxindex
 
 
-def expandtestdatabyones(X_train, y_train):
+def expandtestdatabyones(X_train, y_train,naxcolom,times):
     num_ones= np.sum(y_train)
     Y_train_1=y_train.reshape(y_train.shape[0],1)
     testdata = np.append(X_train,Y_train_1,axis=1)
     # print(X_train.shape)
     # print(testdata.shape)
-    testdata = testdata[(-testdata[:,158]).argsort()]
+    testdata = testdata[(-testdata[:,naxcolom-1]).argsort()]
     ones_testdata=testdata[0:int(num_ones-1),:]
     # print(ones_testdata.shape)
-    for i in xrange(1):
+    for i in xrange(times):
         ones_testdata= np.append(ones_testdata,ones_testdata,axis=0)
     aa=(np.random.rand(ones_testdata.shape[0],ones_testdata.shape[1]) -0.5)
-    diffmat= np.multiply(aa,ones_testdata)*0.0000
+    diffmat= np.multiply(aa,ones_testdata)*0.0023
     ones_testdata=ones_testdata+diffmat
     ones_testdata[:,ones_testdata.shape[1]-1]=1
 
@@ -200,7 +211,7 @@ def expandtestdatabyones(X_train, y_train):
 
 
 
-def createModelEX(coin,datasetMA,naxcolom):
+def createModelEX(coin,datasetMA,naxcolom,buysellmode="Buy"):
 
     # load data
 
@@ -211,7 +222,13 @@ def createModelEX(coin,datasetMA,naxcolom):
     seed = 7
     test_size = 0.33
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
-    # expanded = expandtestdatabyones(X_train, y_train)
+    
+
+    if(buysellmode=="Sell"):
+        expanded = expandtestdatabyones(X_train, y_train,naxcolom,1)
+        X_train=np.append(X_train,expanded[:,0:naxcolom-1],axis=0)
+        y_train=np.append(y_train,expanded[:,naxcolom-1],axis=0)
+
 
     num_ones= np.sum(y_train)
 
@@ -228,54 +245,83 @@ def createModelEX(coin,datasetMA,naxcolom):
     y_pred= model.predict(X_train)
     predictions = [round(value) for value in y_pred]
     accuracy = accuracy_score(predictions, y_train)
-    cm = confusion_matrix(y_train, y_pred)
-    print("Training set results:")
-    print cm
+    cmtrain = confusion_matrix(y_train, y_pred)
+    # print("Training set results:")
+    # print cmtrain
 
 
 
     # make predictions for test data
     y_pred = model.predict(X_test)
+
     predictions = [round(value) for value in y_pred]
     # evaluate predictions
     accuracy = accuracy_score(y_test, predictions)
-    cm = confusion_matrix(y_test, y_pred)
-    np.set_printoptions(precision=4)
+    cmtest = confusion_matrix(y_test, y_pred)
+    
     print("Test set results:")
-    print cm
-    #print(model.feature_importances_)
+    print cmtest
+    # print(model.feature_importances_)
     # save model
     pickle.dump(model, open("./model/"+coin+".dat", "wb"))
-    return cm
+    return cmtest,cmtrain
 
 def main(argv):
     import timeit
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    for key in config['modelparams']: 
+        print(key)
+    if(str(config['modelparams']['onlinemode']) =='yes'):
+        onlinemode=True
+    else:
+        onlinemode=False
+
+    modeltype=config['modelparams']['buysellmodel']
+
+
+
+
     start_global_time = timeit.default_timer()
     coins1_partial = ['BTC_ETH' ,'BTC_AMP' ,'BTC_ARDR', 'BTC_BCN','BTC_BCY','BTC_BELA','BTC_BLK']
     coins1 = ['BTC_ETH' ,'BTC_AMP' ,'BTC_ARDR', 'BTC_BCN','BTC_BCY','BTC_BELA','BTC_BLK', 'BTC_BTCD','BTC_BTM','BTC_BTM','BTC_BURST','BTC_CLAM','BTC_DASH','BTC_DCR','BTC_DGB','BTC_DOGE','BTC_EMC2','BTC_ETC','BTC_ETH','BTC_EXP','BTC_FCT','BTC_FLDC','BTC_FLO','BTC_GAME','BTC_GNO']
     coins2 = ['BTC_ETC','BTC_ETH','BTC_EXP','BTC_FCT','BTC_FLDC','BTC_FLO','BTC_GAME','BTC_GNO', 'BTC_GNT','BTC_GRC','BTC_HUC','BTC_LBC','BTC_LSK','BTC_LTC','BTC_MAID','BTC_NAUT','BTC_NAV','BTC_NEOS','BTC_NMC','BTC_NOTE','BTC_NXC']
     coins3 =['BTC_NXT','BTC_PASC','BTC_PINK','BTC_POT','BTC_PPC','BTC_RADS','BTC_REP','BTC_RIC','BTC_SBD','BTC_SC','BTC_SJCX','BTC_STEEM','BTC_STRAT','BTC_STR','BTC_SYS','BTC_VIA','BTC_VRC','BTC_VTC','BTC_XBC','BTC_XCP','BTC_XEM','BTC_XMR']
 
-    coins = coins1 #coins1_partial #coins1+coins2+ coins3#['BTC_BCN']
-    cm =[[0,0],[0,0]]
+    coins = coins1+coins2+ coins3 #'BTC_BCN'] #coins1 #coins1_partial #coins1+coins2+ coins3#['BTC_BCN']
+    cmtrain =[[0,0],[0,0]]
+    cmtest =[[0,0],[0,0]]
     for coin in coins:
         start = timeit.default_timer()
-        startTime = calendar.timegm(time.strptime('01/04/2017', '%d/%m/%Y'))
-        endTime = calendar.timegm(time.strptime('30/06/2017', '%d/%m/%Y'))
+        startTime = calendar.timegm(time.strptime('01/03/2017', '%d/%m/%Y'))
+        endTime = calendar.timegm(time.strptime('30/07/2017', '%d/%m/%Y'))
 
         # create csv data file with all features and Y
-        dataset,naxcolom = createCoinDataEX(coin,startTime,endTime)
+        dataset,naxcolom = createCoinDataEX(coin,startTime,endTime,mode=onlinemode,buysellmode=modeltype)
         # create xgboost model and save it to a file
-        cm=cm+createModelEX(coin,dataset,naxcolom)
+        ctest,ctrain=createModelEX(coin,dataset,naxcolom,buysellmode=modeltype)
+        cmtrain=cmtrain+ctrain
+        cmtest=cmtest+ctest
+
         #createModel(coin)
 
 
         elapsed = timeit.default_timer() - start
         print("Elepased Time   for ", coin,"    :",elapsed)
-    print("Sum CM")
-    print(cm)
-    print(cm/np.sum(cm))
-    print("Elapsed time for all :", start_global_time-timeit.default_timer())
+    print("  --------------  Total Results ---------")
+    print("Total CM , train and test")
+    print(cmtrain)
+    print(cmtest)
+    print()
+    print("Total CM  ratio , train and test")
+    np.set_printoptions(precision=6)
+    print(cmtrain/np.sum(cmtrain))
+    print()
+    np.set_printoptions(precision=6)
+    print(cmtest/np.sum(cmtest))
+    print("Elapsed time for all :", timeit.default_timer()- start_global_time)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
